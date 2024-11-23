6b913ef46b853fb2f7afc5552b63eaa497e25b2c
ANDREA-ADSO <yulyosorio757@gmail.com>
2024-11-23 14:50:41 -0500

    yey

---
+++ WebAppApiyey/Model/Empleado.cs
@@ -0,0 +1,12 @@
+﻿namespace WebAppApiyey.Model
+{
+    public class Empleado
+    {
+        public int Id { get; set; }
+        public required string Identificacion { get; set; }
+        public required string Nombre { get; set; }
+        public double HorasTrabajadas { get; set; }
+        public double SueldoPorHora { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ WebAppApiyey/Services/IEmpleadoService.cs
@@ -0,0 +1,11 @@
+﻿using WebAppApiyey.Model;
+
+namespace WebAppApiyey.Services
+{
+    public interface IEmpleadoService
+    {
+        List<Empleado> GetAllEmpleados(bool? isActive); Empleado? GetEmpleadoByID(int id);
+        Empleado AddEmpleado(AddUpdateEmpleado obj);
+        Empleado? UpdateEmpleado(int id, AddUpdateEmpleado obj); bool DeleteEmpleadoByID(int id);
+    }
+}
